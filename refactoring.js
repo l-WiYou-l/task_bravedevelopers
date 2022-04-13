bbbf12891d8ec8432ee4d54d83d00801b21d5e3d
WiYou <damir010401@mail.ru>
2022-04-13 17:37:39 +0400

    change refactoring.js

--- refactoring.js
+++ refactoring.js
@@ -1,5 +1,5 @@
 const lastIndexPosition = (string, aIndex, bIndex) => {
-  if (string.length || typeof string !== "number") {
+  if (string.length && typeof string === "string") {
     for (let i = string.length; i > 0; i--) {
       if (string[i] === aIndex || string[i] === bIndex) return i;
     }
